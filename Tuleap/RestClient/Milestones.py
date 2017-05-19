9b33c8181a94480e2e227036a378eaaece19c1a4
moreauxh <humbert.moreaux@enalean.com>
2017-05-19 14:33:16 +0200

    add Milestones module

---
+++ Tuleap/RestClient/Milestones.py
@@ -0,0 +1,48 @@
+import json
+
+from Tuleap.RestClient.Commons import FieldsToFetch, FieldValuesFormat
+
+# Public -------------------------------------------------------------------------------------------
+
+
+class Milestones(object):
+    """
+    Handles "/milestones" methods of the Tuleap REST API.
+
+    Fields type information:
+    :type _connection: Tuleap.RestClient.Connection.Connection
+    :type _data: dict | list[dict]
+    """
+
+    def __init__(self, connection):
+        """
+        Constructor
+
+        :param connection: connection object (must already be logged in)
+        :type connection: Tuleap.RestClient.Connection.Connection
+        """
+        self._connection = connection
+        self._data = None
+
+    def get_data(self):
+        """
+        Get data received in the last response message.
+
+        :return: Response data
+        :rtype: dict | list[dict]
+
+        :note: One of the request method should be successfully executed before this method is
+               called!
+        """
+        return self._data
+
+    def get_last_response_message(self):
+        """
+        Get last response message.
+
+        :return: Last response message
+        :rtype: requests.Response
+
+        :note: This is just a proxy to the connection's method.
+        """
+        self._connection.get_last_response_message()

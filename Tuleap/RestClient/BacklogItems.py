52d570094807de676e1aff38bd86308a66732152
moreauxh <humbert.moreaux@enalean.com>
2017-05-26 11:09:02 +0200

    add copyright and remove unused parameters

--- Tuleap/RestClient/BacklogItems.py
+++ Tuleap/RestClient/BacklogItems.py
@@ -1,5 +1,24 @@
-import json
+"""
+Created on 26.05.2017
 
+:author: Humbert Moreaux
+
+Tuleap REST API Client for Python
+Copyright (c) Humbert Moreaux, All rights reserved.
+
+This Python module is free software; you can redistribute it and/or modify it under the terms of the
+GNU Lesser General Public License as published by the Free Software Foundation; either version 3.0
+of the License, or (at your option) any later version.
+
+This Python module is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this library. If
+not, see <http://www.gnu.org/licenses/>.
+"""
+
+import json
 
 # Public -------------------------------------------------------------------------------------------
 
@@ -35,13 +54,11 @@ class BacklogItems(object):
         """
         return self._data
 
-    def request_backlog_items(self, backlog_item_id, limit=10, offset=None):
+    def request_backlog_items(self, backlog_item_id):
         """
         Request backlog items from the server using the "/backlog_items" method of the Tuleap REST API.
 
         :param int backlog_item_id: Backlog Item ID
-        :param int limit: Optional parameter for maximum limit of returned projects
-        :param int offset: Optional parameter for start index for returned projects
 
         :return: success: Success or failure
         :rtype: bool
@@ -52,17 +69,10 @@ class BacklogItems(object):
 
         # Get backlog items
         relative_url = "/backlog_items/{:}".format(backlog_item_id)
-        parameters = dict()
 
-        if limit is not None:
-            parameters["limit"] = limit
+        success = self._connection.call_get_method(relative_url)
 
-        if offset is not None:
-            parameters["offset"] = offset
-
-        success = self._connection.call_get_method(relative_url, parameters)
-
-        # parse response
+        # Parse response
         if success:
             self._data = json.loads(self._connection.get_last_response_message().text)
 

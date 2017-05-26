23282a866de0bc868b9e7fddcba52a6c3bfdf199
moreauxh <humbert.moreaux@enalean.com>
2017-05-26 10:43:45 +0200

    add copyright

--- Tuleap/RestClient/Milestones.py
+++ Tuleap/RestClient/Milestones.py
@@ -1,3 +1,23 @@
+"""
+Created on 26.05.2017
+
+:author: Moreaux Humbert
+
+Tuleap REST API Client for Python
+Copyright (c) Moreaux Humbert, All rights reserved.
+
+This Python module is free software; you can redistribute it and/or modify it under the terms of the
+GNU Lesser General Public License as published by the Free Software Foundation; either version 3.0
+of the License, or (at your option) any later version.
+
+This Python module is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this library. If
+not, see <http://www.gnu.org/licenses/>.
+"""
+
 import json
 from Tuleap.RestClient.Commons import Order
 
